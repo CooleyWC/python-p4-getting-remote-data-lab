d2bbf76aeee47689127dbf39fc9ca6a479d8e8bc
Will Cooley <william.cooley@gmail.coom>
2024-02-12 11:52:10 -0500

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        data_list = []
+        data = json.loads(self.get_response_body())
+        for element in data:
+            data_list.append(element)
+
+        return data
